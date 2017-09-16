739d01b799e4a7f6306c7d3a323c5e131218c84b
Lera Morozova <tito_alba@mail.ru>
2017-09-16 11:57:46 +0300

    Something happends

---
+++ Class2.py
@@ -0,0 +1,18 @@
+#example, выкачиваем много страниц
+
+import urllib.request
+
+def download_page(pageUrl):
+    try:
+        page = urllib.request.urlopen(pageUrl)
+        print(page.geturl()) #итоговый url после переадресации
+        text = page.read().decode('ISO-8859-1')
+    except:
+        print('Error at', pageUrl)
+        return
+    # do something with the downloaded text
+
+commonUrl = 'http://www.forumishqiptar.com/threads/'
+for i in range(160400, 160425):
+    pageUrl = commonUrl + str(i)
+    download_page(pageUrl)
