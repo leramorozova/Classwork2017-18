4ca1a6cacbbf2dec76f9777fa556569316d4cc24
TATARINOV DMITRY <tito_alba@mail.ru>
2017-09-17 00:45:30 +0300

    Почти, только цикл не циклится

--- Class2.py
+++ Class2.py
@@ -16,3 +16,37 @@ commonUrl = 'http://www.forumishqiptar.com/threads/'
 for i in range(160400, 160425):
     pageUrl = commonUrl + str(i)
     download_page(pageUrl)
+
+
+# Скачать код страницы, найти ссылку на следующий/предыдущий пост,
+# перейти по ней и скачать следующую страницу. И так все - краулер
+
+import urllib.request
+import re
+
+# на входе ссылка на страницу
+
+def download_file(url):
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+    req = urllib.request.Request(url, headers={'User-Agent': user_agent})
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('ISO-8859-1')
+    file_name = url[38:44] + '.txt'
+    page_code = open(file_name, 'w', encoding='utf-8')
+    text = page_code.write(html)
+    return html
+
+# по выполнении данного цикла на выходе ссылка на след страницу + файл с предыдущей
+# на входе код страницы
+def search_link(html):
+    reg = re.search('<!-- next / previous links -->.*?<a href="(.*?)">.*?</a>.*?<!-- / next / previous links -->', html, flags = re.DOTALL)
+    next_link = 'http://www.forumishqiptar.com/' + reg.group(1)
+    return next_link
+
+
+new_link = search_link(download_file('http://www.forumishqiptar.com/threads/162496-Ndihm-per-Windows-7'))
+if new_link!= None:
+    next_page=search_link(download_file(new_link))
+    new_link=next_page
+    search_link(download_file(new_link))
+print('Done')
